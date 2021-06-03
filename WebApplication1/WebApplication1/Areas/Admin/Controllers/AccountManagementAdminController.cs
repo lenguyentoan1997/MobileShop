328b1a899daa65dec34819c4158078dc1cd39093
lenguyentoan1997 <lenguyentoan1997@gmail.com>
2021-06-03 15:04:40 +0700

    Update Account Admin

---
+++ WebApplication1/WebApplication1/Areas/Admin/Controllers/AccountManagementAdminController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.BUS;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    public class AccountManagementAdminController : Controller
+    {
+        // GET: Admin/AccountManagementAdmin
+        public ActionResult Index()
+        {
+            var db = AccountBUS.ListAccount();
+            return View(db);
+        }
+
+        // GET: Admin/AccountManagementAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/AccountManagementAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AccountManagementAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AccountManagementAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AccountManagementAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AccountManagementAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AccountManagementAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/BUS/AccountBUS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopOnlineConnection;
+
+namespace WebApplication1.Models.BUS
+{
+    //----------------Admin--------------
+    public class AccountBUS
+    {
+        public static IEnumerable<AspNetUser> ListAccount()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<AspNetUser>("SELECT Id,Email,PhoneNumber,UserName FROM AspNetUsers");
+
+        }
+
+    }
+}

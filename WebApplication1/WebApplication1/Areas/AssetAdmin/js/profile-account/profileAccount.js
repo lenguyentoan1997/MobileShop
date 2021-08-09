4485bdb9da693fd3e5e764bc84783dc1db17af72
lenguyentoan1997 <lenguyentoan1997@gmail.com>
2021-08-09 17:32:43 +0700

    [FixError] Reorganzie files JavaScript

---
+++ WebApplication1/WebApplication1/Areas/AssetAdmin/js/profile-account/profileAccount.js
@@ -0,0 +1,47 @@
+﻿/*Edit Profile Account*/
+$(document).ready(function () {
+    //Enable input Edit Name
+    $("#btnEditName").click(function () {
+        if (this.value == "Enable") {
+            this.value = "Disable";
+            $("#user-name").prop("readonly", false);
+        }
+        else {
+            this.value = "Enable";
+            $("#user-name").prop("readonly", true);
+        }
+    });
+
+    //Enable input Edit Phone
+    $("#btnEditPhone").click(function () {
+        if (this.value == "Enable") {
+            this.value = "Disable"
+            $("#phone").prop("readonly", false);
+        } else {
+            this.value = "Enable";
+            $("#phone").prop("readonly", true);
+        }
+    });
+
+    //Enable input Edit Address
+    $("#btnEditAddress").click(function () {
+        if (this.value == "Enable") {
+            this.value = "Disable"
+            $("#address").prop("readonly", false);
+        } else {
+            this.value = "Enable";
+            $("#address").prop("readonly", true);
+        }
+    });
+
+    //Enable input Edit Date Of Birthday
+    $("#btnEditDOB").click(function () {
+        if (this.value == "Enable") {
+            this.value = "Disable"
+            $("#dob").prop("readonly", false);
+        } else {
+            this.value = "Enable";
+            $("#dob").prop("readonly", true);
+        }
+    });
+});

cdfdc5a7d2cd63bd5daa499e947c616912b63f93
lenguyentoan1997 <lenguyentoan1997@gmail.com>
2021-08-18 17:41:36 +0700

    [FixError] Display Similar Products

---
+++ WebApplication1/WebApplication1/Asset/js/productDetails01.js
@@ -0,0 +1,43 @@
+﻿/*hover name comment tooltip*/
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+/*Load Comment*/
+document.addEventListener("DOMContentLoaded", function (event) {
+    var scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function (e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
+
+/*Similar Product*/
+$('.center').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
